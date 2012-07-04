fcd7759919579416592c575fdf08910e2eae4eda
davehome <davehome@redthumb.info.tm>
2012-07-04 16:10:20 -0600

    main: use DIE(); get pkg dirs manually.

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <dirent.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,30 +16,39 @@ main(int argc, char** argv)
 	struct dirent *dir_ent;
 	struct stat dir_stat;
 	char *path;
+	size_t path_len;
+	int ret;
 	const char *dir_name;
 	const char* srcpkgs;
 
 	if (argc < 2) {
-		fprintf(stderr, "Error: Must pass srcpkgdir.\n");
-		exit(EXIT_FAILURE);
+		errno = EINVAL;
+		DIE("Must pass srcpkgdir.\nExample:\n\t%s /foo\n", argv[0]);
 	}
 
 	srcpkgs = argv[1];
 
 	dir_p = opendir(srcpkgs);
-
 	if (dir_p == NULL) {
-		fprintf(stderr, "Error: Can't open '%s.\n", srcpkgs);
-		exit(EXIT_FAILURE);
+		DIE("Can't open '%s'.", srcpkgs);
 	}
 
 	while ((dir_ent = readdir(dir_p)) != NULL) {
 		dir_name = dir_ent->d_name;
-	
+
 		if (strcmp(".", dir_name) == 0 || strcmp("..", dir_name) == 0)
 			continue;
 
-		path = get_pkg_dir(dir_name, srcpkgs);
+		path_len = strlen(dir_name) + strlen(srcpkgs) + 2;
+		if ((path = malloc(sizeof(char) * path_len)) == NULL) {
+			DIE("%s", "Can't allocate memory for a string.");
+		}
+
+		ret = snprintf(path, path_len, "%s/%s", srcpkgs, dir_name);
+		if (ret < 1) {
+			DIE("%s", "Output of snprintf() was truncated.");
+		}
+
 		lstat(path, &dir_stat);
 		free(path);
 
