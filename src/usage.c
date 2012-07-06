2c144fc68d5f5302a232d842cf3bfb7a1ef23981
davehome <davehome@redthumb.info.tm>
2012-07-06 06:20:35 -0600

    src/usage.c: use printf() instead of g_fprintf().

--- src/usage.c
+++ src/usage.c
@@ -1,12 +1,11 @@
 #include <stdlib.h>
-#include <glib/gprintf.h>
 
 #include "rcv.h"
 
 void
 rcv_usage(void)
 {
-	g_printf(
+	printf(
 "Usage: %s <dir>\n"
 "Options:\n"
 "\t<dir>\tlocation of the xbps-src source pkgs.\n"
@@ -14,7 +13,7 @@ rcv_usage(void)
 "This program shows which XBPS binary packages need to be rebuilt on your\n"
 "system by comparing the versions of the binary packages which are available \n"
 "in the XBPS repositories registered in your `xbps.conf' with the latest \n"
-"available versions of them in the source package tree\n\n",
+"available versions of them in the source package tree.\n\n",
 		r->progname);
 	rcv_free();
 	exit(EXIT_FAILURE);
