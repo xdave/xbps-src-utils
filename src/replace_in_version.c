d0920f7b3a490baace77284ea1e7a52a1c751e48
davehome <davehome@redthumb.info.tm>
2012-07-07 13:28:34 -0600

    src/replace_in_version.c: use version_key var; don't cast it.

--- src/replace_in_version.c
+++ src/replace_in_version.c
@@ -5,8 +5,8 @@ void
 replace_in_version(GHashTable *ht, const gchar *pat, const gchar *repl)
 {
 	gchar *version_key =  g_strdup("version");
-	gchar *version = g_hash_table_lookup(ht, "version");
+	gchar *version = g_hash_table_lookup(ht, version_key);
 	gchar *new_version = NULL;
 	new_version = str_replace(version, pat, repl);
-	g_hash_table_replace(ht, (gpointer)version_key, new_version);
+	g_hash_table_replace(ht, version_key, new_version);
 }
